5938cc0c574ab1bbeeec3ac7f642cefcc986bb17
zhaoyuanmeng <1426593075@qq.com>
2022-06-02 16:47:40 +0800

    feat: add ha-cli

---
+++ cli/REDEME.md
@@ -0,0 +1,16 @@
+## 手写 CLI 工具
+
+1. 基于 node 实现
+
+## 步骤
+
+1. 初始化 npm init -y
+
+2. 实现窗口命令 zyd create xxx
+
+## 步骤
+- node 会把可执行的文件放到 bin 目录下 然后创建可执行的脚本（zyd）
+- （配置 package.json bin 选项）
+- npm link 链接到本地 （测试的时候可以这么做等到发布到 npm 的时候就不用了）(默认以 name 为基准 也可以配别名表)
+
+> link相当于将当前本地模块链接到npm目录下 
---
+++ cli/lib/create.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const fs = require("fs-extra"); //fs不太靠谱 不支持promise 使用fs-extra
+const Inquirer = require("inquirer");
+// 创建模块
+module.exports = async function (projectName, options) {
+  // 1. 创建项目（考虑同名问题需要覆盖）
+  const cwd = process.cwd(); // 获取工作目录
+  const targetDir = path.join(cwd, projectName); //获取文件完整的路径
+  if (fs.existsSync(targetDir)) {
+    if (options.force) {
+      await fs.remove(targetDir);
+    } else {
+      // 提示用户是否去欸的那个要覆盖
+      let { action } = await Inquirer.prompt([
+        {
+          name: "action", //于结构出的对象名一样
+          type: "list", //类型非常多
+          message: "选择一个动作把",
+          choices: [
+            { name: "Overwrite", value: "overwrite" },
+            { name: "离开", value: false },
+          ],
+        },
+      ]);
+      switch (action) {
+        case false:
+          return;
+        case "overwrite":
+          console.log("\r\n当前正在删除中");
+          await fs.remove(targetDir);
+          console.log("\r\n删除成功");
+          break;
+      }
+    }
+  }
+  // 创建项目
+};
