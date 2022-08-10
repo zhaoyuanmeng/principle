20c13be04831fdf4b73e673692b95add941b57ea
zhaoyuanmeng <1426593075@qq.com>
2022-08-10 18:36:21 +0800

    feat:添加ts emmu

--- ts-project/src/main.ts
+++ ts-project/src/main.ts
@@ -13,7 +13,38 @@ import func from "./func";
 
 import type { gg } from "./func";
 
-console.log("msg", message);
+import { DictQueryCriteria } from "./emum";
+
+type Test<Type> = {
+  -readonly [Property in keyof Type]: string;
+};
+type res = typeof DictQueryCriteria;
+
+type demo = Test<res>;
+
+type AAAA<T> = {
+  code?: string[];
+  dictType: T;
+  status?: boolean;
+};
+
+let a: AAAA<demo> = {
+  code: ["adad"],
+  dictType: {
+    VENDIBILITY_STATUS: "VENDIBILITY_STATUS", // 可卖状态
+    SALES_AREA: "SALES_AREA", // 销售组织
+    BARGAIN_PRIORITY: "BARGAIN_PRIORITY", // 议价优先级
+    BIG_ORDER_REPORT: "BIG_ORDER_REPORT", // 是否允许大单报备
+    MGMT_AREA: "MGMT_AREA", // 管理分区
+    BARGAIN_SCENARIO: "BARGAIN_SCENARIO", // 议价场景
+    CUSTOMER_AREA: "CUSTOMER_AREA", // 客户处
+    PRODUCT_SCOPE: "PRODUCT_SCOPE", // 产品域
+    SALES_METHOD: "SALES_METHOD", // 销售方式
+    CUSTOMER_ATTRIBUTION: "CUSTOMER_ATTRIBUTION", // 客户归属
+    CTO_TYPE: "CTO_TYPE", // ctoType 类型
+  },
+  status: false,
+};
 
 const fn = (text: string) => {
   console.log("---", text);
