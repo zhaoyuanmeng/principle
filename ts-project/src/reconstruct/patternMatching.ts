769a49ecad43046253a827990e8c0f3eda94b7ae
zhaoyuanmeng <1426593075@qq.com>
2022-09-23 11:36:10 +0800

    feat: 添加获取数组第一个和最后一个元素

--- ts-project/src/reconstruct/patternMatching.ts
+++ ts-project/src/reconstruct/patternMatching.ts
@@ -1,3 +1,14 @@
 // 模式匹配做提取
 
+type GetArrOne<Arr extends unknown[]> = Arr extends [infer One, ...unknown[]]
+  ? One
+  : never;
+
+type GetArrLast<Arr extends unknown[]> = Arr extends [...unknown[], infer Last]
+  ? Last
+  : never;
+
+type one = GetArrOne<[1, 2]>;
+type Last = GetArrLast<[1, 2]>;
+
 export const a = "a";
