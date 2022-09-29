f3590e66741cb84e11f2cf384cc09f51bb1aa95c
zhaoyuanmeng <1426593075@qq.com>
2022-09-29 17:19:29 +0800

    feat: 添加获取函数返回值

---
+++ ts-project/src/reconstruct/getParameters.ts
@@ -0,0 +1,18 @@
+/*
+ *@Author: 赵元达
+ *@Date: 2022-09-29 11:33:55
+ *@parms:
+ *@Description: 提取参数和返回值的类型。
+ */
+
+type GetReturnType<Fn extends Function> = Fn extends (
+  ...args: any[]
+) => infer ReturType
+  ? ReturType
+  : never;
+
+type fn = (a: string) => string;
+
+type MyReturn = GetReturnType<fn>;
+
+export let test = "a";
