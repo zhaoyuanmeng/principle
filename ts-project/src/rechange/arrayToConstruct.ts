5e8b2d541e659d19d75d3267c6c9d38b1fef973a
zhaoyuanmeng <1426593075@qq.com>
2022-10-02 19:37:59 +0800

    feat: add array construct

--- ts-project/src/rechange/arrayToConstruct.ts
+++ ts-project/src/rechange/arrayToConstruct.ts
@@ -2,12 +2,7 @@ export type Push<Arr extends unknown[], Element> = [...Arr, Element];
 
 export type Unshift<Arr extends unknown[], Element> = [Element, ...Arr];
 
-//合并数组
-
-type PushRes = Push<[1, 2, 3], "aaa">;
-
-type unShiftRes = Unshift<[1, 2, 3], "bbb">;
-
+//合并数组 非递归版
 type Zip<ArrOne extends unknown[], ArrTwo extends unknown[]> = [
   ...ArrOne
 ] extends [infer oneArrOneVal, infer oneArrTwoVal]
@@ -16,8 +11,29 @@ type Zip<ArrOne extends unknown[], ArrTwo extends unknown[]> = [
     : never
   : never;
 
+//合并数组 递归版
+type ZipPlus<
+  ArrOne extends unknown[],
+  ArrTwo extends unknown[]
+> = ArrOne extends [infer oneArrOneVal, ...infer oneArrOtherVal]
+  ? ArrTwo extends [infer twoArrOneVal, ...infer twoArrOtherVal]
+    ? [[oneArrOneVal, twoArrOneVal], ...ZipPlus<oneArrOtherVal, twoArrOtherVal>]
+    : []
+  : [];
+
+type PushRes = Push<[1, 2, 3], "aaa">;
+
+type unShiftRes = Unshift<[1, 2, 3], "bbb">;
+
 type ResZip = Zip<[1, "zyd"], [2, "zpp"]>;
+
+type ResPlusZip = ZipPlus<[1, 2], ["a", "b"]>;
 
 let pushRes: PushRes = [1, 2, 3, "aaa"];
 
 let unShiftRess: unShiftRes = ["bbb", 1, 2, 3];
+
+let test: ResPlusZip = [
+  [1, "a"],
+  [2, "b"],
+];
